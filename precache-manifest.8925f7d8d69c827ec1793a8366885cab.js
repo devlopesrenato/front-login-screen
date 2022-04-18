860806ed29775b499c7c4205dd3156237d07a3cb
devlopesrenato <renatolopes37@gmail.com>
2022-04-18 11:19:57 -0300

    Updates

---
+++ precache-manifest.8925f7d8d69c827ec1793a8366885cab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4ef48bd86614f093ae5a",
+    "url": "/static/css/main.92261083.chunk.css"
+  },
+  {
+    "revision": "4ef48bd86614f093ae5a",
+    "url": "/static/js/main.4ef48bd8.chunk.js"
+  },
+  {
+    "revision": "e5e41b36236d87d8987a",
+    "url": "/static/js/1.e5e41b36.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2e032d44a1ceb1e0b895f690bd74cc0e",
+    "url": "/index.html"
+  }
+];
